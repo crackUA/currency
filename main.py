d37247ca101d3cabbe70c23c08c56ad4d4fcf0ff
Davyd <103603698+crackUA@users.noreply.github.com>
2022-04-20 21:53:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time # 𝐌𝐨𝐝𝐮𝐥𝐞 𝐭𝐨 𝐬𝐭𝐨𝐩 𝐭𝐡𝐞 �
 # 𝐌𝐚𝐢𝐧 𝐜𝐥𝐚𝐬𝐬 
 class Currency:
 	# 𝐘𝐨𝐮𝐫 𝐩𝐚𝐫𝐬𝐞 𝐩𝐚𝐠𝐞 𝐥𝐢𝐧𝐤. 
-	DOLLAR_KRN = 'https://www.google.com/search?q=dollar+UAH&client=opera-gx&hs=bdo&sxsrf=APq-WBvBFS8B7wgFwq2XULI-lPVeXLylKg%3A1649886063378&ei=b0NXYqLbFseX8gL49LngBQ&ved=0ahUKEwjizujFgJL3AhXHi1wKHXh6DlwQ4dUDCA0&uact=5&oq=dollar+UAH&gs_lcp=Cgdnd3Mtd2l6EAMyDAgAEAoQywEQRhCCAjIFCAAQgAQyBwgAEAoQywEyBwgAEAoQywEyBwgAEAoQywEyBQgAEIAEMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB46BwgjELADECc6BwgAEEcQsAM6BwgAELADEEM6BQgAEMsBOgQIABAeOgkIABDJAxAKEB46BwgjELACECc6BAgAEA06BAgjECc6CQgAEAoQywEQKjoECAAQCkoECEEYAEoECEYYAFC3EFixIWDVImgEcAF4AIABYIgBigWSAQE4mAEAoAEByAEKwAEB&sclient=gws-wiz'
+	DOLLAR_UAH = 'https://www.google.com/search?q=dollar+UAH&client=opera-gx&hs=bdo&sxsrf=APq-WBvBFS8B7wgFwq2XULI-lPVeXLylKg%3A1649886063378&ei=b0NXYqLbFseX8gL49LngBQ&ved=0ahUKEwjizujFgJL3AhXHi1wKHXh6DlwQ4dUDCA0&uact=5&oq=dollar+UAH&gs_lcp=Cgdnd3Mtd2l6EAMyDAgAEAoQywEQRhCCAjIFCAAQgAQyBwgAEAoQywEyBwgAEAoQywEyBwgAEAoQywEyBQgAEIAEMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB46BwgjELADECc6BwgAEEcQsAM6BwgAELADEEM6BQgAEMsBOgQIABAeOgkIABDJAxAKEB46BwgjELACECc6BAgAEA06BAgjECc6CQgAEAoQywEQKjoECAAQCkoECEEYAEoECEYYAFC3EFixIWDVImgEcAF4AIABYIgBigWSAQE4mAEAoAEByAEKwAEB&sclient=gws-wiz'
 	# 𝐇𝐞𝐚𝐝𝐞𝐫 𝐭𝐨 𝐟𝐥𝐚𝐬𝐡 𝐰𝐢𝐭𝐡 𝐔𝐑𝐋. 𝐘𝐨𝐮𝐫 𝐮𝐬𝐞𝐫 𝐚𝐠𝐞𝐧𝐭, 𝐲𝐨𝐮 𝐜𝐚𝐧 𝐟𝐢𝐧𝐝 𝐢𝐭 𝐟𝐨𝐥𝐥𝐨𝐰 𝐭𝐡𝐢𝐬 𝐥𝐢𝐧𝐤 𝐡𝐭𝐭𝐩://𝐦𝐲-𝐮𝐬𝐞𝐫-𝐚𝐠𝐞𝐧𝐭.𝐜𝐨𝐦. 
 	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59'}
 
@@ -17,7 +17,7 @@ class Currency:
 	# 𝐌𝐞𝐭𝐡𝐨𝐝 𝐭𝐨 𝐩𝐚𝐫𝐬𝐞 𝐩𝐚𝐠𝐞.
 	def get_currency_price(self):
 		# 𝐏𝐚𝐫𝐬𝐞 𝐰𝐡𝐨𝐥𝐞 𝐩𝐚𝐠𝐞. 
-		full_page = requests.get(self.DOLLAR_KRN, headers=self.headers)
+		full_page = requests.get(self.DOLLAR_UAH, headers=self.headers)
 
 		# 𝐁𝐞𝐚𝐮𝐭𝐢𝐟𝐮𝐥𝐒𝐨𝐮𝐩 𝐚𝐧𝐚𝐥𝐲𝐬𝐢𝐬.
 		soup = BeautifulSoup(full_page.content, 'html.parser')
